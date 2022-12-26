b1237d235738b0422ec2f139018d12b8415bf1ca
iuddin <8118095+iuddin@users.noreply.github.com>
2022-12-25 23:41:44 -0800

    3rd and 4th stage

--- app/main.js
+++ app/main.js
@@ -3,11 +3,13 @@ const net = require("net");
 console.log("Program logs!");
 
 const server = net.createServer((connection) => {
-  // Handle connection
+  // Handle connection: 
   // Add event listener for the "data" event
   connection.on("data", () => {
-    //Send hard-coded response to client (since already known that req is PING)
+    //Send hard-coded response to client (since already known that sample req is PING)
     connection.write("+PONG\r\n")
+    //In JS, listening for the data event will also automatically handle/respond to multiple PING commands sent by the same connection/client
+    //Since the concurrency model is based on event loop in JS, above code will handle multiple concurrent clients
   });
 });
 
