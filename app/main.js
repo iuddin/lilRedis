78211525d7f58449a0e6d4fe1265ec758be51a7a
iuddin <8118095+iuddin@users.noreply.github.com>
2022-12-27 13:15:57 -0800

    Stage 5 step

--- app/main.js
+++ app/main.js
@@ -1,20 +1,20 @@
 const net = require("net");
 
-console.log("Program logs!");
+//Set up the Redis server (using Node.js net package)
+//Respond to multiple PINGs: connection.on handles it (since listening to the `data` event)
+  //No need to run a loop that reads input from connection
+//createServer (or listening for data event in JS) will also automatically handle/respond to multiple PING commands sent by the same connection/client
+//Handle concurrent connection: createServer handles concurrency
+  //No need to use threads or implement an event loop
 
 const server = net.createServer((connection) => {
-  // Handle connection: 
-  // Add event listener for the "data" event
-  connection.on("data", () => {
-    //Send hard-coded response to client (since already known that sample req is PING)
-    connection.write("+PONG\r\n")
-    //In JS, listening for the data event will also automatically handle/respond to multiple PING commands sent by the same connection/client
-    //Since the concurrency model is based on event loop in JS, above code will handle multiple concurrent clients
+  connection.on("data", (data) => {
+    const dataInString = data.toString();
+    console.log('data after convert byte data in Buffer object to str', dataInString);
   });
-
-  connection.on("clientCommandData", () => {
-    connection.write(`+${clientCommandData}\r\n`)
-  })
+  
 });
+
+
 
 server.listen(6379, "127.0.0.1");
